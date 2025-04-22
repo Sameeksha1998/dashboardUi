f61c14c155f753fee9289964070ed0fb1e4fd8e4
sameeksha.sihare <sameeksha.sihare@aspiresoftserv.com>
2025-04-22 17:50:36 +0530

    update code for metrics

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <Router> {/* Use HashRouter for GitHub Pages */}
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
